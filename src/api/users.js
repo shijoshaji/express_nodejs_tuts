c76875feec996c4d463419193b92c349cb899db1
Shijo Shaji <jojovms@gmail.com>
2024-02-24 12:49:48 +0530

    installing Passport

--- src/api/users.js
+++ src/api/users.js
@@ -7,8 +7,8 @@ const userRoute = Router();
 const API_PATH = '/api/users';
 
 userRoute.get(`${API_PATH}/getAllUser`, (req, res) => {
-  console.log(req.session)
-  console.log(req.sessionID)
+  console.log(req.session);
+  console.log(req.sessionID);
   return res.send(mockUsers);
 });
 
